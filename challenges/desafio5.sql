f277416f8ee1530eef34f407557dcade74b72d5d
ferreirajessica <samojess@gmail.com>
2023-03-06 20:45:49 -0300

    finaliza des 05

---
+++ challenges/desafio5.sql
@@ -0,0 +1,6 @@
+SELECT  s.title AS cancao,	  
+       COUNT(h.song_id) AS reproducoes
+FROM history AS h
+JOIN song AS s ON s.id = h.song_id
+GROUP BY h.song_id
+ORDER BY COUNT(h.song_id) DESC, title ASC LIMIT 2;
